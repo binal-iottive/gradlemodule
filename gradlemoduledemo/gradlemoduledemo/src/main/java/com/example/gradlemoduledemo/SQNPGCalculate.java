64aed900a47afc5f87e2db41f0db50e1ff844046
DESKTOP-QG4DPA5\Admin <binal.patel@iottive.com>
2021-05-17 19:06:56 +0530

    add a class to libraray

---
+++ gradlemoduledemo/gradlemoduledemo/src/main/java/com/example/gradlemoduledemo/SQNPGCalculate.java
@@ -0,0 +1,21 @@
+package com.example.gradlemoduledemo;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Arrays;
+
+public class SQNPGCalculate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sqnpg_calculate);
+    }
+
+    public static int getLLAData() {
+        return 1;
+    }
+}
